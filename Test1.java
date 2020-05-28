cf6bfd2ff7ed300f9d42c09ccc9bc495fd6ac6c8
rlakidi <ravik4428@gmail.com>
2020-05-28 14:34:22 +0530

    Sample code

---
+++ Test1.java
@@ -0,0 +1,13 @@
+public class Test1{
+	public static void main(String[] args) {
+
+		for(int i=0; i<10; i++){
+		System.out.println("First Demo    :     "+i);
+		}
+		
+		for(int i=0; i<10; i++){
+		System.out.println("secound Demo    :     "+i);
+		}
+	}
+}
+
