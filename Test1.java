52c0f3b7643e4c811f0f532aa69d6c42c527cdf8
rlakidi <ravik4428@gmail.com>
2020-05-28 14:47:33 +0530

    Removing addtional Loops

--- Test1.java
+++ Test1.java
@@ -4,19 +4,6 @@ public class Test1{
 		for(int i=0; i<10; i++){
 		System.out.println("First Demo    :     "+i);
 		}
-		
-		for(int i=0; i<10; i++){
-		System.out.println("secound Demo    :     "+i);
-		}
-		
-		
-		for(int i=0; i<10; i++){
-		System.out.println("third Demo    :     "+i);
-		}
-		
-		for(int i=0; i<10; i++){
-		System.out.println("four Demo    :     "+i);
-		}
 	}
 }
 
